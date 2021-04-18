d804da169a625a71700ab5453b7a0a55b41b4d2a
BudacaB <budaca.b@gmail.com>
2021-04-19 00:14:38 +0300

    tasks

--- Lecture3/lecture3/hello/views.py
+++ Lecture3/lecture3/hello/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
+    # return HttpResponse("Hello!")
+
 
 def bog(request):
     return HttpResponse("Hello, Bog!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()  # context provided for the template
+    })
+    # return HttpResponse(f"Hello, {name.capitalize()}!")
