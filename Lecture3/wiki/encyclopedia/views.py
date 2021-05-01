b0b2b97dc01677068b16ec65fdcc2eef1fc938d8
BudacaB <budaca.b@gmail.com>
2021-05-01 22:49:52 +0300

    wiki entry and index

--- Lecture3/wiki/encyclopedia/views.py
+++ Lecture3/wiki/encyclopedia/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def title(request, title):
+    markdowner = Markdown()
+    # check get_entry output
+    if util.get_entry(title) is None:
+        content = f'{title} Not Found'
+    else:
+        content = markdowner.convert(util.get_entry(title))
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title,
+        "content": content
     })
 
