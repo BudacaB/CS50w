49f23f4216d7d8251312ffe9fb10c04c0b6850d3
BudacaB <budaca.b@gmail.com>
2021-05-06 01:23:26 +0300

    search

--- Lecture3/wiki/encyclopedia/views.py
+++ Lecture3/wiki/encyclopedia/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 from . import util
 from markdown2 import Markdown
+from django import forms
+from django.http import HttpResponseRedirect
+
+class NewSearchForm(forms.Form):
+    search = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 def title(request, title):
@@ -17,6 +23,27 @@ def title(request, title):
         content = markdowner.convert(util.get_entry(title))
     return render(request, "encyclopedia/wiki.html", {
         "title": title,
-        "content": content
+        "content": content,
+        "form": NewSearchForm()
     })
 
+def search(request):
+    if request.method == 'POST':
+        form = NewSearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data["search"]
+            if util.get_entry(search) is None:
+                entries = util.list_entries()
+                filtered_entries = [k for k in entries if search in k]
+                if not filtered_entries:
+                    return render(request, "encyclopedia/search.html", {
+                        "search": search,
+                        "form": NewSearchForm()
+                    })
+                else:
+                    return render(request, "encyclopedia/index.html", {
+                        "entries": filtered_entries,
+                        "form": NewSearchForm()
+                    })
+            else:
+                return HttpResponseRedirect('/wiki/' + search)
