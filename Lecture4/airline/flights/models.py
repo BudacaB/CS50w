58b86d1e5e7ddfb5b8db128682c3b82c5265993e
BudacaB <budaca.b@gmail.com>
2021-07-27 10:57:16 +0300

    airport model and table

--- Lecture4/airline/flights/models.py
+++ Lecture4/airline/flights/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures") # related_name is a reverse relationship, we can not only get the flight's origin from Airport, but also all the flights from Flights that have an airport as an origin
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
 
