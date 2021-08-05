8fb73bdafb22033d03a7332fee4b05a0f9f653ef
BudacaB <budaca.b@gmail.com>
2021-08-05 23:40:41 +0300

    mode model work

--- Lecture4/commerce/auctions/models.py
+++ Lecture4/commerce/auctions/models.py
@@ -12,12 +12,21 @@ class Listing(models.Model):
     category = models.CharField(max_length=64)
     listed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
 
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+
 class Bid(models.Model):
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, related_name="bid_objects")
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="bids")
     bid = models.FloatField()
 
+    def __str__(self):
+        return f"{self.bidder}"
+
 class Comment(models.Model):
     comment = models.CharField(max_length=128)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="comments")
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, blank=True, related_name="comments")
+
+    def __str__(self):
+        return f"{self.user}"
