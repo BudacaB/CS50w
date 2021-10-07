8572a49061abe18c5946ddb9509ed347deae29a2
BudacaB <budaca.b@gmail.com>
2021-10-08 00:42:56 +0300

    even more js

--- Lecture5/counter.js
+++ Lecture5/counter.js
@@ -4,12 +4,14 @@ function count() {
     counter++;
     document.querySelector('h1').innerHTML = counter;
 
-    if (counter % 10 === 0) {
-        alert(`Count is now ${counter}`);
-    }
+    // if (counter % 10 === 0) {
+    //     alert(`Count is now ${counter}`);
+    // }
 }
 
 // DOM loading is top to bottom - need to find the button element if not using its tag properties
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button').onclick = count;
+
+    setInterval(count, 1000);
 });
