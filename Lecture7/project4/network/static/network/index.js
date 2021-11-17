cc3bb828e021ec8445c9c30c66dc04ed396c9772
BudacaB <budaca.b@gmail.com>
2021-11-17 14:31:02 +0200

    all posts partially done 2

--- Lecture7/project4/network/static/network/index.js
+++ Lecture7/project4/network/static/network/index.js
@@ -30,14 +30,31 @@ document.addEventListener('DOMContentLoaded', function() {
                         <h5>${element.user}</h5>
                         <p>${element.post}</p>
                         <p>${element.created}</p>
-                        <button class="heart">&#9825;</button>
+                        <button class="heart" onclick="clicked(this)" id="${element.user}${element.id}">&#9825;</button>
                         <span>${element.likes} likes</span>
                     </div>
                 </div>
             </div>
         </div>`
-            // postsDiv.append(post);
         });
     })
     .catch(error => console.log('Error', error));
 })
+
+function clicked(elem) {
+    console.log('clicked ' + elem.id);
+    const whiteHeart = '\u2661';
+    const blackHeart = '\u2764';
+    const button = document.querySelector('#' + elem.id);
+    button.addEventListener('click', function toggle() {
+        console.log('clicked alright');
+        const like = button.textContent;
+        if(like==whiteHeart) {
+            button.textContent = blackHeart;
+            // button.className = 'red_heart';
+        } else {
+            button.textContent = whiteHeart;
+            // button.className = '';
+        }
+    });
+}
