02fb97cb68f0c35b38f565b82288bc53a081bafb
BudacaB <budaca.b@gmail.com>
2021-11-17 17:37:11 +0200

    all posts done

--- Lecture7/project4/network/static/network/index.js
+++ Lecture7/project4/network/static/network/index.js
@@ -1,60 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // const whiteHeart = '\u2661';
-    // const blackHeart = '\u2764';
-    // const button = document.querySelector('.heart');
-    // button.addEventListener('click', function toggle() {
-    //     console.log('clicked');
-    //     const like = button.textContent;
-    //     if(like==whiteHeart) {
-    //         button.textContent = blackHeart;
-    //         button.className = 'red_heart';
-    //     } else {
-    //         button.textContent = whiteHeart;
-    //         button.className = '';
-    //     }
-    // });
+    getPosts();
+})
 
+function getPosts() {
     fetch('/all_posts')
     .then(response => response.json())
     .then(result => {
         console.log(result)
         const postsDiv = document.querySelector('#posts');
         result.forEach(element => {
-            console.log(element);
-            // const post = document.createElement('div');
-            // post.innerHTML = "test";
-            postsDiv.innerHTML += `<div class="card post">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12">
-                        <h5>${element.user}</h5>
-                        <p>${element.post}</p>
-                        <p>${element.created}</p>
-                        <button class="heart" onclick="clicked(this)" id="${element.user}${element.id}">&#9825;</button>
-                        <span>${element.likes} likes</span>
+            postsDiv.innerHTML += 
+                `<div class="card post">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <h5>${element.user}</h5>
+                                <p>${element.post}</p>
+                                <p>${element.created}</p>
+                                <button class="heart" onclick="clicked(this)" id="${element.user}${element.id}">&hearts;</button>
+                                <span>${element.likes} likes</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>`
+                </div>`
+            const button = document.querySelector(`#${element.user}${element.id}`);
+            button.addEventListener('click', clicked);
         });
     })
     .catch(error => console.log('Error', error));
-})
+}
 
 function clicked(elem) {
-    console.log('clicked ' + elem.id);
-    const whiteHeart = '\u2661';
-    const blackHeart = '\u2764';
-    const button = document.querySelector('#' + elem.id);
-    button.addEventListener('click', function toggle() {
-        console.log('clicked alright');
-        const like = button.textContent;
-        if(like==whiteHeart) {
-            button.textContent = blackHeart;
-            // button.className = 'red_heart';
-        } else {
-            button.textContent = whiteHeart;
-            // button.className = '';
-        }
-    });
+    const like = elem.textContent;
+    if (elem.className == 'heart') {
+        elem.className = 'heart red_heart';
+    } else {
+        elem.className = 'heart';
+    }
 }
