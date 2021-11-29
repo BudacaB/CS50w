c7c63b6e7f078d97ef9e3a4f055087de0c149aa8
BudacaB <budaca.b@gmail.com>
2021-11-29 18:00:23 +0200

    edit done

--- Lecture7/project4/network/static/network/edit.js
+++ Lecture7/project4/network/static/network/edit.js
@@ -1,22 +1,43 @@
-function edit(postId) {
+function editIndex(postId) {
     document.querySelector(`#post${postId}`).style.display = 'none';
     document.querySelector(`#form${postId}`).removeAttribute('hidden');
 }
 
-function saveEdit(postId) {
-    let updatedPost = document.querySelector(`#textarea${postId}`).value;
-    fetch(`/update/${postId}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            post: updatedPost
-        })
-      })
-      .then(response => response.json())
-      .then(result => {
-        document.querySelector(`#post${postId}`).innerHTML = result.updatedPost;
-        document.querySelector(`#post${postId}`).style.display = 'block';
-        document.querySelector(`#form${postId}`).hidden = true;
-      })
-      .catch(error => console.log('Error', error));
-    
+function editProfile(postId) {
+    document.querySelector(`#profilePost${postId}`).style.display = 'none';
+    document.querySelector(`#profileForm${postId}`).removeAttribute('hidden');
+}
+
+function saveEdit(postId, location) {
+    if (location == '') {
+        let updatedPost = document.querySelector(`#textarea${postId}`).value;
+        fetch(`/update/${postId}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                post: updatedPost
+            })
+          })
+          .then(response => response.json())
+          .then(result => {
+            document.querySelector(`#post${postId}`).innerHTML = result.updatedPost;
+            document.querySelector(`#post${postId}`).style.display = 'block';
+            document.querySelector(`#form${postId}`).hidden = true;
+          })
+          .catch(error => console.log('Error', error));
+    } else {
+        let updatedPost = document.querySelector(`#profileTextarea${postId}`).value;
+        fetch(`/update/${postId}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                post: updatedPost
+            })
+          })
+          .then(response => response.json())
+          .then(result => {
+            document.querySelector(`#profilePost${postId}`).innerHTML = result.updatedPost;
+            document.querySelector(`#profilePost${postId}`).style.display = 'block';
+            document.querySelector(`#profileForm${postId}`).hidden = true;
+          })
+          .catch(error => console.log('Error', error));
+    }
 }
